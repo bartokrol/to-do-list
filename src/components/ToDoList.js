4aa436a929c13cb23b1647e69923c6dfdc072e19
Bartosz Król <barto.kro@gmail.com>
2021-07-21 22:46:32 +0200

    create toDoList__inputsBox

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
 function ToDoList() {
-	return <div className="ToDoList"></div>;
+	const [taskHeader, setTaskHeader] = useState("");
+	const [taskDesc, setTaskDesc] = useState("");
+
+	return (
+		<div className="ToDoList">
+			<div className="ToDoList__inputsBox">
+				<input
+					type="text"
+					className="ToDoList__inputsBox__headingInput"
+					value={taskHeader}
+				/>
+				<textarea
+					type="text"
+					className="ToDoList__inputsBox__descriptionInput"
+					value={taskDesc}
+				/>
+				<button>Add</button>
+			</div>
+		</div>
+	);
 }
 
 export default ToDoList;
