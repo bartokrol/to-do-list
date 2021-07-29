2e7e33fb07f6fbd37e79958618f262d711400e84
Bartosz Król <barto.kro@gmail.com>
2021-07-29 16:43:50 +0200

    start to create DoneTasks.js structure

--- src/layouts/DoneTasks.js
+++ src/layouts/DoneTasks.js
@@ -1,8 +1,29 @@
-const DoneTasks = ({ toDolistClassName }) => {
+import Task from "./Task";
+
+const DoneTasks = ({ completedTasks, toDolistClassName }) => {
 	const doneTasksClassName = `${toDolistClassName}__doneTasks`;
+	const completedTasksArr = completedTasks.map((task) => (
+		<>
+			<h1>{task.taskHeader}</h1>
+			<h2>{task.taskDesc}</h2>
+		</>
+	));
+	//
+	// <Task
+	// 	taskNumber={task.taskNumber}
+	// 	header={task.taskHeader}
+	// 	desc={task.taskDesc}
+	// 	visible={task.taskVisible}
+	// 	taskClassName={tasksClassName}
+	// 	taskOpenCloseClick={openCloseBtn}
+	// 	taskDeleteClick={deleteBtn}
+	// 	taskEditClick={editBtn}
+	// 	taskConfirmClick={confirmBtn}
+	// />
 	return (
 		<div className={doneTasksClassName}>
 			<h1 className={`${doneTasksClassName}__title`}>Done Tasks</h1>
+			{completedTasksArr}
 		</div>
 	);
 };
